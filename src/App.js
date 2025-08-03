7ff8a76f99acf9d474a6197a8aaee3b544e23de3
Rayman Huang <rayman.huang@mail.utoronto.ca>
2025-08-03 18:44:57 -0400

    Clean up unused imports and configure GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-import { FaGithub, FaLinkedin, FaExternalLinkAlt } from 'react-icons/fa';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 import './App.css';
 
 const AppContainer = styled.div`
